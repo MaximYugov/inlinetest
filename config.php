c76b468de4a69932efa04cecf86369c85abecbc7
maximyugov <78841025+maximyugov@users.noreply.github.com>
2022-09-05 20:32:01 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 define('POSTS_URL', 'https://jsonplaceholder.typicode.com/posts');
 define('COMMENTS_URL', 'https://jsonplaceholder.typicode.com/comments');
 
+define('DB_DRIVER', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'inlinetest');
 define('DB_USER', 'root');
