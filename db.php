1c09e6de4bf3a1f798305f07f34929a6cf3c5993
maximyugov <78841025+maximyugov@users.noreply.github.com>
2022-09-05 20:32:37 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('config.php');
+
+/**
+ * @return PDO подключение к БД
+ */
+function connectToDb(): PDO
+{
+	try {
+		$db = new PDO(DB_DRIVER.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
+	} catch (PDOException $e) {
+		print "Ошибка подключения к базе данных: " . $e->getMessage();
+		die();
+	}
+	
+	return $db;
+}
+
+function execute(PDO $db, string $sql, array $values): bool
+{
+	$preparedStatement = $db->prepare($sql);
+    return $preparedStatement->execute($values);
+}
