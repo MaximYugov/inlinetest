c3c705a2903a9c46f0b6f2f8735c8a5132a5faa2
maximyugov <78841025+maximyugov@users.noreply.github.com>
2022-09-05 20:37:12 +0300

    Update db.php

--- db.php
+++ db.php
@@ -7,18 +7,18 @@ require_once('config.php');
  */
 function connectToDb(): PDO
 {
-	try {
-		$db = new PDO(DB_DRIVER.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
-	} catch (PDOException $e) {
-		print "Ошибка подключения к базе данных: " . $e->getMessage();
-		die();
-	}
+    try {
+        $db = new PDO(DB_DRIVER.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
+    } catch (PDOException $e) {
+        print "Ошибка подключения к базе данных: " . $e->getMessage();
+        die();
+    }
 
-	return $db;
+    return $db;
 }
 
 function execute(PDO $db, string $sql, array $values): bool
 {
-	$preparedStatement = $db->prepare($sql);
-	return $preparedStatement->execute($values);
+    $preparedStatement = $db->prepare($sql);
+    return $preparedStatement->execute($values);
 }
